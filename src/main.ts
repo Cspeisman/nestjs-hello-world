16b2b09e67d6c718b276109d4c0197a89f7632f8
Cspeisman <cspeisman@gmail.com>
2018-06-25 13:35:22 -0400

    middleware to serve static files

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { renderFile } from 'ejs'
-import {join} from "path";
+import { renderFile } from 'ejs';
+
+import {join} from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.engine('html', renderFile)
     .set('view engine', 'html')
-    .setBaseViewsDir(join(__dirname, '../resources'))
-    .useStaticAssets(join(__dirname, '../resources'), {
+    .setBaseViewsDir(join(__dirname, '../client/build'))
+    .useStaticAssets(join(__dirname, '../client/build'), {
       index: false,
-      redirect: false
+      redirect: false,
   });
 
-  await app.listen(process.env.PORT || 3000)
+  app.use((req, res, next) => {
+    if (req.url !== '/') {
+      res.sendFile(join(__dirname, '../client/build/index.html'));
+    } else {
+      next();
+    }
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
